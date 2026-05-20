1f7d141c0dbe2c053c59439ae01d37c7df0668e9
eytanbentsvi <2026.e.bentsvi@shalhevet.org>
2026-05-20 11:19:19 -0700

    commit change

--- databases/config.py
+++ databases/config.py
@@ -2,7 +2,6 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 class DevConfig:
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'scanner_rdb.db')}"
