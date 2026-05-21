ded136af8a4d7428936a51f5883fb666ed003fe8
eytanbentsvi <2026.e.bentsvi@shalhevet.org>
2026-05-21 14:59:19 -0700

    push for vercel

--- databases/user_rdb.py
+++ databases/user_rdb.py
@@ -11,8 +11,9 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    dob = db.Column(db.Date, nullable=False)
+    dob = db.Column(db.Date, nullable=True)
     verified = db.Column(db.Boolean, default=False)
+    deleted = db.Column(db.Boolean, default=False)
 
     def __init__(self, username, first_name, last_name, password, email, dob):
         self.username = username
